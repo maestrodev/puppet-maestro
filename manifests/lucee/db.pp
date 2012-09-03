e810bd416ba52317af1f3b396866a1079d2575e5
Brett Porter <brett@apache.org>
2012-09-02 22:53:54 -0700

    fix reference to username

--- manifests/lucee/db.pp
+++ manifests/lucee/db.pp
@@ -19,7 +19,7 @@ class maestro::lucee::db(
       "set /files/etc/maestro_lucee.json/dict//entry[.='database']//entry[.='server']/string ${type}",
       "set /files/etc/maestro_lucee.json/dict//entry[.='database']//entry[.='host']/string ${host}",
       "set /files/etc/maestro_lucee.json/dict//entry[.='database']//entry[.='port']/number ${port}",
-      "set /files/etc/maestro_lucee.json/dict//entry[.='database']//entry[.='user']/string ${user}",
+      "set /files/etc/maestro_lucee.json/dict//entry[.='database']//entry[.='user']/string ${username}",
       "set /files/etc/maestro_lucee.json/dict//entry[.='database']//entry[.='pass']/string ${password}",
       "set /files/etc/maestro_lucee.json/dict//entry[.='database']//entry[.='database_name']/string ${database}",
     ],
