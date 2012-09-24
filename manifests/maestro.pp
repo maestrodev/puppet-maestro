04662df0435ca67ed5622118f4f1cc0ae31c6b42
Brett Porter <brett@apache.org>
2012-09-24 17:01:21 +1000

    Revert "split standalone config from the reusable pieces"
    This reverts commit 82659c267046f33524a2ef4b6a9f9502d7ae9c73.

--- manifests/maestro.pp
+++ manifests/maestro.pp
@@ -119,10 +119,7 @@ class maestro::maestro( $repo = $maestrodev_repo,
   }
 
   class { 'maestro::maestro-postgres':  } ->   
-  class { 'maestro::maestro::package': } -> 
-  class { 'maestro::maestro::config': } -> 
-  class { 'maestro::maestro::jettyconfig': } -> 
-  class { 'maestro::maestro::service': }
+  class { 'maestro::maestro::package': } -> class { 'maestro::maestro::config': } -> class { 'maestro::maestro::service': }
 
   
 }
