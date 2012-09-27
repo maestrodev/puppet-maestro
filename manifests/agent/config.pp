bf7852ecd1cb8efea2dc04d39faa2bdc21fcbe88
Etienne Pelletier <epelletier@maestrodev.com>
2012-09-27 10:22:09 -0500

    Added missing files needed for tarball install refactoring.

---
+++ manifests/agent/config.pp
@@ -0,0 +1,59 @@
+class maestro::agent::config(
+  $srcdir = $maestro::agent::srcdir,
+  $basedir = $maestro::agent::basedir,
+  $agent_user_home = $maestro::agent::agent_user_home,
+  $agent_user = $maestro::agent::agent_user,
+  $agent_group = $maestro::agent::agent_group,
+  $maxmemory = $maestro::agent::maxmemory,
+  $timestamp_version = $maestro::agent::timestamp_version,
+  $facter = $maestro::agent::facter,
+  $stomp_host = $maestro::agent::stomp_host,
+  $maven_servers = $maestro::agent::maven_servers,
+  $agent_name = $maestro::agent::agent_name) {
+  
+  $wrapper = "$basedir/conf/wrapper.conf"
+  case $::operatingsystem {
+     'Darwin': {
+       $java_home="/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home"
+       $managehome = false
+       $sed_suffix = " ''"
+     }
+     default: {
+       $sed_suffix = ""
+       $java_home="/usr/lib/jvm/java"
+       $managehome = true
+     }
+   }
+  
+  file { "maestro_agent.json":
+    path    => "${agent_user_home}/conf/maestro_agent.json",
+    content => template("maestro/agent/maestro_agent.json.erb"),
+    owner   => $agent_user,
+    group   => $agent_group,
+    notify  => Service["maestro-agent"],
+  } ->
+  file { "${basedir}/conf/maestro_agent.json":
+    ensure  => link,
+    target  => "${agent_user_home}/conf/maestro_agent.json",
+  }
+  
+  exec { 'maestro-agent-memory-max':
+    command => "sed -i${sed_suffix} 's/^#wrapper\\.java\\.maxmemory=.*$/wrapper\\.java\\.maxmemory=${maxmemory}/' ${wrapper}",
+    unless  => "egrep '^wrapper.java.maxmemory=${maxmemory}' ${wrapper}",
+    notify  => Service['maestro-agent'],
+  }
+  
+  file { "maestro-agent":
+    path    => "${basedir}/bin/maestro_agent",
+    owner   => $agent_user,
+    group   => $agent_group,
+    mode    => 755,
+    content => template("maestro/agent/maestro-agent.erb"),
+  }
+  
+  file { "${srcdir}/maestro-agent.version":
+    content => "${timestamp_version}\n",
+  }
+
+  
+}
---
+++ manifests/agent/package/tarball.pp
@@ -0,0 +1,30 @@
+class maestro::agent::package::tarball(
+  $repo,
+  $base_version,
+  $timestamp_version,
+  $srcdir,
+  $basedir,
+  $agent_user,
+  $agent_group,
+  $agent_user_home) {
+  
+    wget::authfetch { "fetch-agent":
+      user        => $repo['username'],
+      password    => $repo['password'],
+      source      => "${repo['url']}/com/maestrodev/lucee/agent/${base_version}/agent-${timestamp_version}-bin.tar.gz",
+      destination => "$srcdir/agent-${timestamp_version}-bin.tar.gz",
+      require     => File[$srcdir],
+    } ->
+    exec {"rm -rf $basedir":
+       unless => "egrep \"^${timestamp_version}$\" $srcdir/maestro-agent.version",
+    } ->
+    exec { "unpack-agent":
+      command => "tar zxf $srcdir/agent-${timestamp_version}-bin.tar.gz && chown -R ${agent_user}:${agent_group} maestro-agent ${agent_user_home}",
+      creates => "${basedir}/lib",
+      cwd     => '/usr/local', # TODO use $basedir instead of hardcoded
+      path    => "/bin/:/usr/bin:/usr/sbin",
+      require => [User[$agent_user],File[$agent_user_home]],
+      notify  => Service["maestro-agent"],
+    }
+  
+}
