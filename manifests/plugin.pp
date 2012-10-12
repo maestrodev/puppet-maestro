33a535b87a9b0133969684ced4ae417a143e7245
Carlos Sanchez <csanchez@maestrodev.com>
2012-10-12 11:47:10 +0200

    Verify that plugins are correctly installed by Maestro

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -13,6 +13,9 @@ define maestro::plugin($version) {
     $base_version = $version
   }
 
+  $plugin_folder = "${user_home}/.maestro/plugins"
+  $plugin_file = "${name}-${version}-bin.zip"
+
   # download the plugin to /usr/local/src
   if ! defined(File['/usr/local/src']) {
     file {'/usr/local/src':
@@ -29,7 +32,19 @@ define maestro::plugin($version) {
   } ->
 
   # copy to .maestro/plugins if it hasn't been installed already
-  exec { "cp /usr/local/src/${name}-${version}-bin.zip ${user_home}/.maestro/plugins/${name}-${version}-bin.zip":
-    unless => "test -s ${user_home}/.maestro/plugins/installed/${name}-${version}-bin.zip",
+  exec { "cp /usr/local/src/${name}-${version}-bin.zip ${plugin_folder}/${plugin_file}":
+    unless => "test -s ${plugin_folder}/installed/${plugin_file}",
+  } ->
+
+  # verify that plugin has been installed correctly in Maestro
+  exec { "wait-plugin-installed-${name}":
+    command   => "test -s ${plugin_folder}/installed/${plugin_file} || test -s ${plugin_folder}/failed/${plugin_file}",
+    tries     => 30,
+    try_sleep => 4,
+    require => [Exec['startup_wait']],
+  } ->
+  exec { "assert-plugin-installed-${name}":
+    command   => "test -s ${plugin_folder}/installed/${plugin_file}",
   }
+
 }
