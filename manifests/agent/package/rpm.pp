a63fd255d8bf90b11aeffec316b78f0ff355e529
Carlos Sanchez <csanchez@maestrodev.com>
2012-10-15 18:17:24 +0200

    Download the rpm once as puppet executes rpm -q and downloads it every time

--- manifests/agent/package/rpm.pp
+++ manifests/agent/package/rpm.pp
@@ -1,20 +1,27 @@
 class maestro::agent::package::rpm (
-    $repo,
-    $timestamp_version,
-    $base_version)
+  $repo,
+  $timestamp_version,
+  $base_version)
 {
 
-    if $repo['url'] =~ /^(https:\/\/)(.*)$/ {
-      $uri = "${2}"
-    } 
+  if $repo['url'] =~ /^(https:\/\/)(.*)$/ {
+    $uri = "${2}"
+  } 
 
-    # When we have a proper yum repo, this variable can go away.
-     $maestro_agent_source = "https://${repo['username']}:${repo['password']}@${uri}/com/maestrodev/lucee/agent/${base_version}/agent-${timestamp_version}-rpm.rpm"
+  # When we have a proper yum repo, this variable can go away.
+  $maestro_agent_source = "https://${uri}/com/maestrodev/lucee/agent/${base_version}/agent-${timestamp_version}-rpm.rpm"
 
-    package { 'maestro-agent':
-     ensure => latest,
-     source => $maestro_agent_source,
-     provider => rpm,
-    }
+  wget::authfetch { "fetch-agent-rpm":
+    user        => $repo['username'],
+    password    => $repo['password'],
+    source      => $maestro_agent_source,
+    destination => "/usr/local/src/agent-${timestamp_version}.rpm",
+    require     => File['/usr/local/src'],
+  } ->
+  package { 'maestro-agent':
+    ensure   => latest,
+    source   => "/usr/local/src/agent-${timestamp_version}.rpm",
+    provider => rpm,
+  }
 
 }
--- manifests/maestro/package/rpm.pp
+++ manifests/maestro/package/rpm.pp
@@ -1,8 +1,7 @@
 class maestro::maestro::package::rpm(
   $repo,
   $version,
-  $base_version,
-  )
+  $base_version)
 {
 
   if $repo['url'] =~ /^(https:\/\/)(.*)$/ {
@@ -10,12 +9,19 @@ class maestro::maestro::package::rpm(
   } 
   
   # When we have a proper yum repo, this variable can go away.
-   $maestro_source = "https://${repo['username']}:${repo['password']}@${uri}/com/maestrodev/maestro/rpm/maestro/${base_version}/maestro-${version}.rpm"
+  $maestro_source = "https://${uri}/com/maestrodev/maestro/rpm/maestro/${base_version}/maestro-${version}.rpm"
   
+  wget::authfetch { "fetch-maestro-rpm":
+    user        => $repo['username'],
+    password    => $repo['password'],
+    source      => $maestro_source,
+    destination => "/usr/local/src/maestro-${version}.rpm",
+    require     => File['/usr/local/src'],
+  } ->
   package { 'maestro':
-   ensure => latest,
-   source => $maestro_source,
-   provider => rpm,
+    ensure   => latest,
+    source   => "/usr/local/src/maestro-${version}.rpm",
+    provider => rpm,
   }
   
 }
