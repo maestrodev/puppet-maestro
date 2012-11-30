7f3496901d24de8784cce5e0508efe02189f494a
Carlos Sanchez <csanchez@maestrodev.com>
2012-11-30 12:29:54 +0100

    Update plugin versions

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -5,19 +5,19 @@ class maestro::plugins() {
     version => '1.2',
   }
   maestro::plugin { 'maestro-continuum-plugin':
-    version => '1.5-20121031.080048-5',
+    version => '1.5',
   }
   maestro::plugin { 'maestro-scm-plugin':
-    version => '1.0-20120710.051326-11',
+    version => '1.0-20121129.185735-13',
   }
   maestro::plugin { 'maestro-jenkins-plugin':
-    version => '1.1-20121025.194334-2',
+    version => '1.1',
   }
   maestro::plugin { 'maestro-bamboo-plugin':
     version => '1.0-20121025.131245-1',
   }
   maestro::plugin { 'maestro-fog-plugin':
-    version => '1.1-20121004.151511-1',
+    version => '1.1',
   }
 
 }
