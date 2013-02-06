3d0446cf5d946f72d59bdf58d43c0ce95333c4c8
Carlos Sanchez <csanchez@maestrodev.com>
2013-02-06 12:55:48 +0100

    Set plugin versions as parameters so they can be overriden by hiera

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,26 +1,34 @@
 # Maestro default plugins
-class maestro::plugins() {
+# define versions as parameters so they can be set from hiera
+class maestro::plugins(
+  $irc = '1.2',
+  $continuum = '1.5',
+  $scm = '1.0',
+  $jenkins = '1.1.2',
+  $jira = '1.0',
+  $bamboo = '1.1',
+  $fog = '1.3') {
 
   maestro::plugin { 'maestro-irc-plugin':
-    version => '1.2',
+    version => $irc,
   }
   maestro::plugin { 'maestro-continuum-plugin':
-    version => '1.5',
+    version => $continuum,
   }
   maestro::plugin { 'maestro-scm-plugin':
-    version => '1.0',
+    version => $scm,
   }
   maestro::plugin { 'maestro-jenkins-plugin':
-    version => '1.1.2',
+    version => $jenkins,
   }
   maestro::plugin { 'maestro-jira-plugin':
-    version => '1.0',
+    version => $jira,
   }
   maestro::plugin { 'maestro-bamboo-plugin':
-    version => '1.1',
+    version => $bamboo,
   }
   maestro::plugin { 'maestro-fog-plugin':
-    version => '1.3',
+    version => $fog,
   }
 
 }
