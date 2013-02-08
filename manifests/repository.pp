034ab1ea8c5fa86b82ebad6b25d15278e924b41d
Brett Porter <brett@apache.org>
2013-02-08 18:39:22 +1100

    remove the unused id to avoid confusion with the mirror

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -1,7 +1,6 @@
 class maestro::repository($username, $password) {
 
   $maestrodev = {
-    id       => 'maestro-mirror',
     username => $username,
     password => $password,
     url      => 'https://repo.maestrodev.com/archiva/repository/all',
