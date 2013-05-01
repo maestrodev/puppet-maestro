54fdc7aaf63835239031431cb25e4d545679ddae
Brett Porter <brett@maestrodev.com>
2013-05-01 11:38:02 +1000

    update to latest version of libraries
    update for puppet3 compatibility

--- manifests/agent/absent.pp
+++ manifests/agent/absent.pp
@@ -1,7 +1,7 @@
 class maestro::agent::absent(
   $version = '0.1.0',
   $user = 'agent',
-  $user_home = "/home/$user",
+  $user_home = undef,
   $basedir = '/var/maestro-agent') {
 
   service { 'maestro-agent':
@@ -10,13 +10,18 @@ class maestro::agent::absent(
   }
 
   if ! defined(User[$user]) {
+    if $user_home == undef {
+      $user_home_real = "/home/$user"
+    } else {
+      $user_home_real = $user_home
+    }
 
     user { $user:
       ensure     => absent,
       managehome => true,
       require    => Service['maestro-agent'],
     } ->
-    file { $user_home:
+    file { $user_home_real:
       ensure  => absent,
       recurse => true,
       force   => true,
