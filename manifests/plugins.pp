6e44a025c7596448d21881a21f212610d9dbfca4
Doug Henderson <dhenderson@maestrodev.com>
2013-07-23 10:58:20 -0700

    MAESTRO-3030 Updated puppet

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -4,22 +4,31 @@
 # supplied in most Puppet sites to ease updating
 class maestro::plugins(
   $plugins = {
+    'maestro-ant-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
+    'maestro-archive-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
     'maestro-bamboo-plugin' => { version => '1.1' },
     'maestro-continuum-plugin' => { version => '1.6.1' },
     'maestro-cucumber-plugin' => { version => '1.0.1' },
     'maestro-flowdock-plugin' => { version => '1.0' },
     'maestro-fog-plugin' => { version => '1.8' },
     'maestro-gemfury-plugin' => { version => '1.0' },
+    'maestro-git-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
     'maestro-gitblit-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
+    'maestro-httputils-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
     'maestro-irc-plugin' => { version => '1.2' },
     'maestro-jenkins-plugin' => { version => '1.4.4' },
     'maestro-jira-plugin' => { version => '1.0' },
+    'maestro-maven-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
     'maestro-puppet-plugin' => { version => '1.1' },
+    'maestro-rake-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
     'maestro-rightscale-plugin' => { version => '1.0' },
     'maestro-rpm-plugin' => { version => '1.1' },
     'maestro-scm-plugin' => { version => '1.0' },
+    'maestro-shell-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
     'maestro-ssh-plugin' => { version => '1.0' },
     'maestro-sonar-plugin' => { version => '1.0.1', dir => 'com/maestrodev/maestro/plugins' },
+    'maestro-svn-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
+    'maestro-tomcat-plugin' => { version => '1.0', dir => 'com/maestrodev/maestro/plugins' },
   } ) {
   create_resources('maestro::plugin', $plugins)
 }
